926f5d75e493f2c706100215b69823596d3b78ba
enzzotj <enzzojaco@gmail.com>
2023-08-28 17:30:05 -0300

    Inicio projeto

---
+++ src/main/java/webminds/group/pet_backend/PetBackEndApplication.java
@@ -0,0 +1,13 @@
+package webminds.group.pet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetBackEndApplication.class, args);
+	}
+
+}
